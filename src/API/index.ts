2a8086a346fff2f9cd060d2c5e84379d2f518ba7
SashaGitHub01 <sashab4b1n@yandex.ru>
2022-05-05 15:44:42 +0300

    netlifyfix

--- src/API/index.ts
+++ src/API/index.ts
@@ -4,7 +4,7 @@ import axios from 'axios'
 //    'X-RapidAPI-Host': 'coinranking1.p.rapidapi.com',
 //    'X-RapidAPI-Key': '7e8ffc4634mshfaec826692124afp1438e6jsn275cfa1b7274'
 //  }
-
+console.log(process.env.REACT_APP_COINRANKING_URL)
 export const coinrankingApi = axios.create({
    baseURL: process.env.REACT_APP_COINRANKING_URL as string,
    headers: {
